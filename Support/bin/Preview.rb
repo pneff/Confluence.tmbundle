c0c0a70d16d95e2c9c7aacc26790d574f2195864
Patrice Neff <mail@patrice.ch>
2009-06-17 12:41:49 +0200

    Do more Confluence markup replacements in preview. Very hackish but works for now.

--- Support/bin/Preview.rb
+++ Support/bin/Preview.rb
@@ -7,4 +7,35 @@ rescue LoadError
   require 'redcloth'
 end
 
-puts(RedCloth.new($stdin.readlines.join()).to_html(:textile))
+contents = $stdin.readlines.join()
+
+# Get all the noformat strings out so they don't interfere
+noformat = []
+contents.gsub!(/\{noformat\}(.*)\{noformat\}/m) do |match|
+    pl = "#noformatplaceholder#" + noformat.length.to_s
+    noformat.push($1)
+    pl
+end
+
+
+## Replace links with Textile ones
+# Internal links (replace with dummy)
+contents.gsub!(/\[([^|\]]+)\]/, '"\1":http://dummy/\1')
+# External links
+contents.gsub!(/\[([^|\]]+)\|([^ \]]+)\]/, '"\1":\2')
+
+# Workaround: Remove some Confluence tags that cause problems in Textile
+contents.gsub!(/\{table-plus[^}]*\}/, '')
+contents.gsub!(/\{excerpt[^}]*\}/, '')
+
+# Textile
+html = RedCloth.new(contents).to_html(:textile)
+
+# Put the noformat strings back in
+noformat.each_index do |idx|
+    nf = noformat[idx]
+    nf = '<pre>' + nf + '</pre>'
+    html.gsub!("#noformatplaceholder#" + idx.to_s, nf)
+end
+
+puts html
